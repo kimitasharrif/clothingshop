5f93a1d494384018d1318326fb45069e2a419f0a
kimitasharrif <kimitasharrif@gmail.com>
2024-10-20 19:44:39 +0300

    doing something

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import Cart from './Pages/Cart';
 import ShopCategory from './Pages/ShopCategory';
 import LoginSgnup from './Pages/LoginSgnup';
 import Footer from './components/Footer/Footer';
+import men_banner from './components/Assets/banner_mens.png'
+import women_banner from './components/Assets/banner_women.png'
+import kid_banner from './components/Assets/banner_kids.png'
 
 
 function App() {
@@ -17,9 +20,9 @@ function App() {
       <div className="App">
         <Routes>
           <Route index element={<Shop/>} />
-          <Route path="mens" element={<ShopCategory category="men" />} />
-          <Route path="womens" element={<ShopCategory category="women" />} />
-          <Route path="kids" element={<ShopCategory category="kid" />} />
+          <Route path="mens" element={<ShopCategory banner={men_banner} category="men" />} />
+          <Route path="womens" element={<ShopCategory banner={women_banner} category="women" />} />
+          <Route path="kids" element={<ShopCategory banner={kid_banner} category="kid" />} />
           <Route path="product" element={<Product />} />
           <Route path=":productId" element={<Product />} />
           <Route path="cart" element={<Cart/>} />
--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React ,{useContext} from 'react';
+import './CSS/ShopCategory.css';
+import { ShopContext } from '../Context/ShopContext'
+import drop_icon from './components/Assets/drop_icon.png'
 
-const ShopCategory = () => {
+const ShopCategory = (props) => {
+  const {all_product} = useContext(ShopContext);
   return (
-    <div>
-      
+    <div className='shop-category'>
+      <img src={props.banner} alt="" />
+      <div className="shopcategory-indexSort">
+        <p><span>
+          Showing 1-12</span> out of 36 products</p>
+          <div className="shopcategory-sort">
+            Sort by<img src={drop_icon} alt="" />
+          </div>
+      </div>
+      <div className="shopcategory-products">
+        {all_product.map(()=>{
+          
+        })}
+      </div>
     </div>
   )
 }
